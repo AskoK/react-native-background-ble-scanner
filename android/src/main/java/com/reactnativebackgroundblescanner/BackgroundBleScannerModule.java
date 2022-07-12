b02dc0894aea357103a826d2696167b34fb25cad
AskoK <askokrimann@hotmail.com>
2022-07-12 15:57:57 +0300

    imports

--- android/src/main/java/com/reactnativebackgroundblescanner/BackgroundBleScannerModule.java
+++ android/src/main/java/com/reactnativebackgroundblescanner/BackgroundBleScannerModule.java
@@ -6,6 +6,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.widget;
+import android.widget.Toast;
+import android.content.Context;
 
 public class BackgroundBleScannerModule extends ReactContextBaseJavaModule {
 
