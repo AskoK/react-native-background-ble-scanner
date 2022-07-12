9c49cd6dd0f18835d6799f399196eea4df50da4d
AskoK <askokrimann@hotmail.com>
2022-07-12 16:01:53 +0300

    remove import

--- android/src/main/java/com/reactnativebackgroundblescanner/BackgroundBleScannerModule.java
+++ android/src/main/java/com/reactnativebackgroundblescanner/BackgroundBleScannerModule.java
@@ -6,7 +6,6 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
-import android.widget;
 import android.widget.Toast;
 import android.content.Context;
 
